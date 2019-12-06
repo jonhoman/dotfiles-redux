99b0acab826411762ed9bb8382eb6229019629af
Jon Homan <jhoman@hatchloyalty.com>
2019-12-06 16:18:17 -0600

    use Python3 by default

--- fish/config.fish
+++ fish/config.fish
@@ -5,6 +5,8 @@ set -x -g TERM "xterm-256color"
 set -x -g LC_ALL en_GB.UTF-8
 set -x -g LANG en_GB.UTF-8
 
+set -x PATH /usr/local/opt/python/libexec/bin/ $PATH
+
 # rbenv init
 status --is-interactive; and source (rbenv init -|psub)
 
