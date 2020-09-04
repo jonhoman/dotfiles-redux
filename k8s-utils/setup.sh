b74cda3f36505b9f5e4bce7f26c9d3f39a495c1f
Jon Homan <jon@jonhoman.com>
2020-09-04 11:10:18 -0500

    download k8s utils automatically
    ignore all k8s-utils but setup.sh

---
+++ k8s-utils/setup.sh
@@ -0,0 +1,30 @@
+#! /usr/bin/env sh
+
+DIR=$(dirname "$0")
+cd "$DIR"
+
+. ../scripts/functions.sh
+
+info "Setting up k8s utils..."
+
+SOURCE="$(realpath .)"
+DESTINATION="$(realpath ~/bin)"
+
+# TODO: download kubectl, kubectx, kubens from GH/google
+# gitignore kubectx and kubenx
+# symlink
+
+KUBECTX_VERSION="v0.9.1"
+KUBECTL_VERSION="v1.15.12"
+wget -q "https://github.com/ahmetb/kubectx/releases/download/$KUBECTX_VERSION/kubens" -O $SOURCE/kubens
+wget -q "https://github.com/ahmetb/kubectx/releases/download/$KUBECTX_VERSION/kubectx" -O $SOURCE/kubectx
+wget -q "https://storage.googleapis.com/kubernetes-release/release/$KUBECTL_VERSION/bin/darwin/amd64/kubectl" -O $SOURCE/kubectl
+
+chmod +x kube*
+
+mkdir -p "$DESTINATION/"
+symlink "$SOURCE/kubens" "$DESTINATION/kubens"
+symlink "$SOURCE/kubectx" "$DESTINATION/kubectx"
+symlink "$SOURCE/kubectl" "$DESTINATION/kubectl"
+
+success "Finished setting up k8s utils."
