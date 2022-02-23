80b7e21dce4ef674032030a4a6e614385f549c29
Jon Homan <jon@jonhoman.com>
2022-02-23 09:20:37 -0600

    update kubectx and kubens

--- k8s-utils/setup.sh
+++ k8s-utils/setup.sh
@@ -10,7 +10,7 @@ info "Setting up k8s utils..."
 SOURCE="$(realpath .)"
 DESTINATION="$(realpath ~/bin)"
 
-KUBECTX_VERSION="v0.9.1"
+KUBECTX_VERSION="v0.9.4"
 KUBECTL_VERSION="v1.15.12"
 wget -q "https://github.com/ahmetb/kubectx/releases/download/$KUBECTX_VERSION/kubens" -O $SOURCE/kubens
 wget -q "https://github.com/ahmetb/kubectx/releases/download/$KUBECTX_VERSION/kubectx" -O $SOURCE/kubectx
