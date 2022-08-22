768441478438ffe9f694300c3e5b2dbf5239614b
Jon Homan <jon@jonhoman.com>
2022-08-22 08:41:15 -0500

    homebrew cleanup with M1

--- packages/setup.sh
+++ packages/setup.sh
@@ -11,6 +11,6 @@ sudo -v
 
 info "Installing Brewfile packages..."
 brew bundle
-brew link --force awscli@1
+# brew link --force awscli@1
 yes | $(brew --prefix)/opt/fzf/install
 success "Finished installing Brewfile packages."
