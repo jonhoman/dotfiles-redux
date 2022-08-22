aadc75fcba8b5e3c354fdc1577278c9f01d202df
Jon Homan <jon@jonhoman.com>
2022-08-22 08:40:18 -0500

    update kubectl version

--- k8s-utils/setup.sh
+++ k8s-utils/setup.sh
@@ -11,7 +11,7 @@ SOURCE="$(realpath .)"
 DESTINATION="$(realpath ~/bin)"
 
 KUBECTX_VERSION="v0.9.4"
-KUBECTL_VERSION="v1.15.12"
+KUBECTL_VERSION="v1.22.12"
 wget -q "https://github.com/ahmetb/kubectx/releases/download/$KUBECTX_VERSION/kubens" -O $SOURCE/kubens
 wget -q "https://github.com/ahmetb/kubectx/releases/download/$KUBECTX_VERSION/kubectx" -O $SOURCE/kubectx
 wget -q "https://storage.googleapis.com/kubernetes-release/release/$KUBECTL_VERSION/bin/darwin/amd64/kubectl" -O $SOURCE/kubectl
