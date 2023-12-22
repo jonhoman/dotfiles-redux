95bf2456c0947c63eb57cae36ad9521a3d8c3b5a
Jon Homan <jon@jonhoman.com>
2023-12-22 12:30:51 -0600

    update k8s util versions

--- k8s-utils/setup.sh
+++ k8s-utils/setup.sh
@@ -10,8 +10,8 @@ info "Setting up k8s utils..."
 SOURCE="$(realpath .)"
 DESTINATION="$(realpath ~/bin)"
 
-KUBECTX_VERSION="v0.9.4"
-KUBECTL_VERSION="v1.21.12"
+KUBECTX_VERSION="v0.9.5"
+KUBECTL_VERSION="v1.24.17"
 wget -q "https://github.com/ahmetb/kubectx/releases/download/$KUBECTX_VERSION/kubens" -O $SOURCE/kubens
 wget -q "https://github.com/ahmetb/kubectx/releases/download/$KUBECTX_VERSION/kubectx" -O $SOURCE/kubectx
 wget -q "https://storage.googleapis.com/kubernetes-release/release/$KUBECTL_VERSION/bin/darwin/amd64/kubectl" -O $SOURCE/kubectl
